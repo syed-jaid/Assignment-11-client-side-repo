afb86a44aa099c54bbfc77432ffef2ba2c86c611
md-adnan-ahsan <syed.jaid.7748@gmail.com>
2022-05-02 15:58:11 +0600

    navbar is done

--- src/pages/navBar/NavBar.js
+++ src/pages/navBar/NavBar.js
@@ -10,23 +10,22 @@ const NavBar = () => {
             <nav className="navbar navbar-light ">
                 <div className="container-fluid">
                     {/* navbar logo part*/}
-                    <div className='d-flex flex-column ms-3'>
+                    <div className='logo-div d-flex flex-column ms-3'>
                         {/* navbar logo img  */}
-                        <img className='nav-logo ms-1 pb-0' src={logo} alt="" />
+                        <img className='logo-div nav-logo ms-1 pb-0' src={logo} alt="" />
                         {/* navbar logo text  */}
-                        <small className='text-white p-0 m-0'>kawame</small>
+                        <small className='logo-div text-white p-0 m-0'>kawame</small>
                     </div>
                     {/* nav route links  */}
-                    <div>
+                    <div className='nav-rout-links'>
                         <Link className='nav-rout-link' to='/'>Home</Link>
                         <Link className='nav-rout-link' to='/'>Manage Items</Link>
                         <Link className='nav-rout-link' to='/'>Add Item</Link>
                         <Link className='nav-rout-link' to='/'>My items</Link>
-
                     </div>
                     {/* navbar sign in and sign out */}
-                    <div>
-                        <Link to='/'> <button className='sign-out-btn'>Sign Out</button></Link><button className='sign-out-btn'>Sign Out</button>
+                    <div className='sign-div-btn'>
+                        <button className='sign-out-btn'>Sign Out</button>
                     </div>
                 </div>
             </nav>
